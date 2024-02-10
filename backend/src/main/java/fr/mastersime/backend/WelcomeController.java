811237372ea813d89bfe5030e7df69ffb6742658
Riane SMILI <rianesmili@gmail.com>
2024-02-10 19:13:42 +0100

    (backend) initial commit of the backend

---
+++ backend/src/main/java/fr/mastersime/backend/WelcomeController.java
@@ -0,0 +1,13 @@
+package fr.mastersime.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the backend";
+    }
+}
