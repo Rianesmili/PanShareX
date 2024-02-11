cd70c7db2f0f0be10c89cd2acb9df0ab3232a3e2
Riane SMILI <rianesmili@gmail.com>
2024-02-11 02:18:22 +0100

    (pansharex) add NetworkModule to git

---
+++ app/src/main/java/fr/mastersime/pansharex/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package fr.mastersime.pansharex.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    const val BASE_URL = "http://172.20.10.2:8080/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
