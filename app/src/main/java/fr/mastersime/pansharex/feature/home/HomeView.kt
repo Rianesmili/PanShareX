963ec972ebe58c0f4213d565c4dfcdc666568889
Riane SMILI <rianesmili@gmail.com>
2024-02-18 16:00:36 +0100

    update home view

--- app/src/main/java/fr/mastersime/pansharex/feature/home/HomeView.kt
+++ app/src/main/java/fr/mastersime/pansharex/feature/home/HomeView.kt
@@ -11,12 +11,23 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -24,9 +35,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -104,78 +117,112 @@ fun CameraView(navController: NavController, homeViewModel: HomeViewModel) {
         File(it, context.resources.getString(R.string.app_name)).apply { mkdirs() }
     }
 
-    when (isProcessing.value) {
-        true -> {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator() // Affichez un indicateur de progression si isProcessing est true
-            }
-        }
+    Box(modifier = Modifier.fillMaxSize()) {
+        HomeHeader()
+        AndroidView(
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val executor = ContextCompat.getMainExecutor(ctx)
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+                    imageCapture.value = ImageCapture.Builder().build()
+                    bindPreview(
+                        cameraProvider,
+                        previewView,
+                        lifecycleOwner,
+                        imageCapture.value
+                    )
+                }, executor)
+                previewView
+            },  modifier = Modifier
+                .align(Alignment.Center)
+                .padding(bottom = 60.dp)
+                .aspectRatio(1f) //set the aspect ratio to 1:1
+        )
 
-        false -> {
-            Column(modifier = Modifier.fillMaxSize()) {
-                AndroidView(
-                    factory = { ctx ->
-                        val previewView = PreviewView(ctx)
-                        val executor = ContextCompat.getMainExecutor(ctx)
-                        cameraProviderFuture.addListener({
-                            val cameraProvider = cameraProviderFuture.get()
-                            imageCapture.value = ImageCapture.Builder().build()
-                            bindPreview(
-                                cameraProvider,
-                                previewView,
-                                lifecycleOwner,
-                                imageCapture.value
-                            )
-                        }, executor)
-                        previewView
-                    }, modifier = Modifier.fillMaxSize(0.8f)
+        if (isProcessing.value) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center)) // Affichez un indicateur de progression si isProcessing est true
+        } else {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp)
+            ) {
+                CaptureButton(
+                    imageCapture = imageCapture,
+                    outputDirectory = outputDirectory,
+                    context = context,
+                    homeViewModel = homeViewModel,
+                    navController = navController,
+                    isProcessing = isProcessing
                 )
-                Button(onClick = {
-                    if (imageCapture.value != null) {
-                        Log.d("CameraView", "Hello From onCLick Bouton")
-
-                        isProcessing.value = true // Définissez isProcessing à true avant l'appel
-                        CoroutineScope(Dispatchers.Default).launch {
-                            val className = homeViewModel.takePictureAndGetClass(
-                                imageCapture.value,
-                                outputDirectory,
-                                context
-                            )
-                            delay(2000)
-                            isProcessing.value = false // Set isProcessing to false after the call
-                            Log.d("CameraView", "Hello From className: $className")
-
-
-                            withContext(Dispatchers.Main) {
-                                navController.navigate(
-                                    SUMMURY_VIEW_ROUTE.replace(
-                                        "{className}",
-                                        className
-                                    )
-                                )
-                            }
-                        }
-
-                    } else {
-                        Log.e("CameraView", "Camera initialization is not complete")
-                    }
-                }, modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(16.dp),
-                    content = {
-                        Text("Détecter le panneau")
-                    })
-                GalleryButton(homeViewModel = homeViewModel, navController = navController, isProcessing = isProcessing)
+                Spacer(modifier = Modifier.padding(4.dp))
+                GalleryButton(
+                    homeViewModel = homeViewModel,
+                    navController = navController,
+                    isProcessing = isProcessing
+                )
             }
         }
     }
-
 }
 
+@Composable
+fun CaptureButton(
+    imageCapture: MutableState<ImageCapture?>,
+    outputDirectory: File?,
+    context: android.content.Context,
+    homeViewModel: HomeViewModel,
+    navController: NavController,
+    isProcessing: MutableState<Boolean>
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = {
+            if (imageCapture.value != null) {
+                Log.d("CameraView", "Hello From onCLick Bouton")
+
+                isProcessing.value = true // Définissez isProcessing à true avant l'appel
+                CoroutineScope(Dispatchers.Default).launch {
+                    val className = homeViewModel.takePictureAndGetClass(
+                        imageCapture.value,
+                        outputDirectory,
+                        context
+                    )
+                    delay(2000)
+                    isProcessing.value = false // Set isProcessing to false after the call
+                    Log.d("CameraView", "Hello From className: $className")
+
+
+                    withContext(Dispatchers.Main) {
+                        navController.navigate(
+                            SUMMURY_VIEW_ROUTE.replace(
+                                "{className}",
+                                className
+                            )
+                        )
+                    }
+                }
+
+            } else {
+                Log.e("CameraView", "Camera initialization is not complete")
+            }
+        }, content = {
+            Icon(Icons.Filled.PlayArrow, contentDescription = "Camera Icon")
+            Text("Détecter le panneau")
+
+        })
+}
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun GalleryButton(navController: NavController, homeViewModel: HomeViewModel, isProcessing: MutableState<Boolean>) {
+fun GalleryButton(
+    navController: NavController,
+    homeViewModel: HomeViewModel,
+    isProcessing: MutableState<Boolean>
+) {
     val context = LocalContext.current
     val launcher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
@@ -202,14 +249,18 @@ fun GalleryButton(navController: NavController, homeViewModel: HomeViewModel, is
             }
         }
     Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+
         onClick = {
             launcher.launch("image/*")
         },
         content = {
+            Icon(Icons.Filled.Add, contentDescription = "Camera Icon")
             Text("Choisir une image depuis la galerie")
+
         },
-        modifier = Modifier
-            .padding(16.dp)
     )
 }
 
@@ -235,5 +286,24 @@ private fun bindPreview(
         }
     } catch (exc: Exception) {
         Log.e("CameraView", "Use case binding failed", exc)
+    }
+}
+
+@Composable
+fun HomeHeader() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = "PANSHARE X",
+            fontSize = 20.sp,
+            color = Color.White,
+            fontStyle = MaterialTheme.typography.bodyMedium.fontStyle,
+            modifier = Modifier
+                .padding(8.dp)
+        )
     }
 }
